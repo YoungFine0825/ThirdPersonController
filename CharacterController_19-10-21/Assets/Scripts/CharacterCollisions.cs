42fa2c2e24f99c0ffca836e945728c5309d4bfb9
YangFine0825 <reno0825@hotmail.com>
2019-10-22 16:18:22 +0800

    19-10-22

---
+++ CharacterController_19-10-21/Assets/Scripts/CharacterCollisions.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCollisions 
+{
+
+    public static Vector3 ClosestPointOnSurface(SphereCollider col, Vector3 to)
+    {
+        Vector3 p = Vector3.zero;
+
+        p = to - col.transform.position;
+
+        p.Normalize();
+
+        p *= col.radius * col.transform.localScale.x;
+
+        p += col.transform.position;
+
+        return p;
+    }
+
+
+    public static Vector3 ClosestPointOnSurface(BoxCollider col, Vector3 to)
+    {
+        if (col.transform.rotation == Quaternion.identity)
+        {
+            return col.ClosestPointOnBounds(to);
+        }
+        else
+        {
+            return ClosestOnOBB(col, to);
+        }
+    }
+
+
+    public static bool ClosestPointOnSurface(Collider col, Vector3 to,out Vector3 concatPoint)
+    {
+        bool isConcated = false;
+        if (col is BoxCollider)
+        {
+            concatPoint = CharacterCollisions.ClosestPointOnSurface((BoxCollider)col, to);
+        }
+        else if (col is SphereCollider)
+        {
+            concatPoint = CharacterCollisions.ClosestPointOnSurface((SphereCollider)col, to);
+        }
+        else
+        {
+            concatPoint = Vector3.zero;
+        }
+
+        isConcated = concatPoint != Vector3.zero;
+
+        return isConcated;
+    }
+
+
+
+    private static Vector3 ClosestOnOBB(BoxCollider col, Vector3 to)
+    {
+        Vector3 ret = Vector3.zero;
+
+        Transform ct = col.transform;
+
+        Vector3 localTo = ct.InverseTransformPoint(to);
+
+        localTo -= col.center;
+
+        Vector3 localNorm = new Vector3(
+            Mathf.Clamp(localTo.x, -col.size.x * 0.5f, col.size.x * 0.5f),
+            Mathf.Clamp(localTo.y, -col.size.y * 0.5f, col.size.y * 0.5f),
+            Mathf.Clamp(localTo.z, -col.size.z * 0.5f, col.size.z * 0.5f)
+            );
+
+        localNorm += col.center;
+
+        ret = ct.TransformPoint(localNorm);
+
+        return ret;
+    }
+}
---
+++ CharacterController_19-10-21/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+
+    public float radius = 1f;
+
+    private bool _onConcated = false;
+
+    private Vector3 _concatPoint = Vector3.zero;
+
+
+    void LateUpdate()
+    {
+        _onConcated = false;
+
+        _concatPoint = Vector3.zero;
+
+        bool isConcatedSucceeded = false;
+
+        foreach (Collider col in Physics.OverlapSphere(transform.position, radius))
+        {
+            isConcatedSucceeded = CharacterCollisions.ClosestPointOnSurface(col, transform.position,out _concatPoint);
+
+            if (isConcatedSucceeded)
+            {
+                Vector3 v = transform.position - _concatPoint;
+
+                transform.position += Vector3.ClampMagnitude(v,Mathf.Clamp(radius - v.magnitude,0,radius));
+
+                _onConcated = true;
+            }
+
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = _onConcated ? Color.red : Color.green;
+        Gizmos.DrawWireSphere(transform.position,radius);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(_concatPoint,0.2f);
+    }
+}
